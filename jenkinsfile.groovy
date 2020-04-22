0517cbe3211d82d4e22e58e89bf4d46274db926f
Ilnur Garifullin <ilnur.email@gmail.com>
2020-04-22 12:16:57 +0300

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -9,7 +9,6 @@ def buildAndPublishReleaseFunction = {
         /home/ubuntu/.pyenv/bin/pyenv install --skip-existing 3.6.10
         /home/ubuntu/.pyenv/bin/pyenv install --skip-existing 3.7.7
         /home/ubuntu/.pyenv/bin/pyenv install --skip-existing 3.8.2
-        `/home/ubuntu/.pyenv/bin/pyenv init -`
         /home/ubuntu/.pyenv/bin/pyenv shell 3.6.10 3.7.7 3.8.2
         
         python -m venv venv
@@ -49,7 +48,6 @@ def buildFunction = {
         /home/ubuntu/.pyenv/bin/pyenv install --skip-existing 3.6.10
         /home/ubuntu/.pyenv/bin/pyenv install --skip-existing 3.7.7
         /home/ubuntu/.pyenv/bin/pyenv install --skip-existing 3.8.2
-        `/home/ubuntu/.pyenv/bin/pyenv init -`
         /home/ubuntu/.pyenv/bin/pyenv shell 3.6.10 3.7.7 3.8.2
         
         python -m venv venv
