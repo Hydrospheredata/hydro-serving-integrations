93309b621114a2e6e0ab3a78009ed20807d65458
Ilnur Garifullin <ilnur.email@gmail.com>
2020-04-22 15:23:41 +0300

    Test env

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,83 +1,97 @@
 def repository = 'hydro-serving-integrations'
 
 def buildAndPublishReleaseFunction = {
-    configFileProvider([configFile(fileId: 'PYPIDeployConfiguration', targetLocation: '.pypirc', variable: 'PYPI_SETTINGS')]) {
-        sh """#!/bin/bash
-        set -ex
+    withCredentials([
+        string(credentialsId: 'HydrosphereDevEndpoint', variable: 'HYDROSPHERE_ENDPOINT'),
+        string(credentialsId: 'HydroIntegrationsAWSRegion', variable: 'AWS_REGION'),
+        string(credentialsId: 'HydroIntegrationsS3DataCaptureBucket', variable: 'S3_DATA_CAPTURE_BUCKET'),
+        string(credentialsId: 'HydroIntegrationsS3DataCapturePrefix', variable: 'S3_DATA_CAPTURE_PREFIX'),
+        string(credentialsId: 'HydroIntegrationsS3DataTrainingBucket', variable: 'S3_DATA_TRAINING_BUCKET'),
+        string(credentialsId: 'HydroIntegrationsS3DataTrainingPrefix', variable: 'S3_DATA_TRAINING_PREFIX'),
+    ]) {
+        configFileProvider([configFile(fileId: 'PYPIDeployConfiguration', targetLocation: '.pypirc', variable: 'PYPI_SETTINGS')]) {
+            sh """#!/bin/bash
+            set -ex
 
-        # prepare environment
-        export AWS_REGION=us-west-3
+            # prepare environment
+            pyenv install --skip-existing 3.6.10
+            pyenv install --skip-existing 3.7.7
+            pyenv install --skip-existing 3.8.2
+            eval "\$(pyenv init -)"
+            pyenv shell 3.6.10 3.7.7 3.8.2
+            
+            python -m venv venv
+            source venv/bin/activate
+            pip install wheel~=0.34.2
+            pip install tox~=3.14.5
 
-        pyenv install --skip-existing 3.6.10
-        pyenv install --skip-existing 3.7.7
-        pyenv install --skip-existing 3.8.2
-        eval "\$(pyenv init -)"
-        pyenv shell 3.6.10 3.7.7 3.8.2
-        
-        python -m venv venv
-        source venv/bin/activate
-        pip install wheel~=0.34.2
-        pip install tox~=3.14.5
+            # run tests for lambda distribution
+            # and for hydro-integrations sdk
+            tox
 
-        # run tests for lambda distribution
-        # and for hydro-integrations sdk
-        tox
+            # build lambda distribution artifacts
+            cd scrips/aws/traffic_shadowing
+            ./sam-build.sh
+            ./sam-package.sh
+            
+            # publish lambda distribution artifacts
+            ./cf-build-upload.sh
+            cd ../../../
+            
+            # build hydro-integrations sdk
+            pip install -r requirements.txt
+            python setup.py bdist_wheel
 
-        # build lambda distribution artifacts
-        cd scrips/aws/traffic_shadowing
-        ./sam-build.sh
-        ./sam-package.sh
-        
-        # publish lambda distribution artifacts
-        ./cf-build-upload.sh
-        cd ../../../
-        
-        # build hydro-integrations sdk
-        pip install -r requirements.txt
-        python setup.py bdist_wheel
-
-        # publish hydro-integrations sdk package
-        python -m twine upload --config-file ${env.WORKSPACE}/.pypirc -r pypi ${env.WORKSPACE}/dist/*
-        deactivate
-    """
+            # publish hydro-integrations sdk package
+            python -m twine upload --config-file ${env.WORKSPACE}/.pypirc -r pypi ${env.WORKSPACE}/dist/*
+            deactivate
+            """
+        }
     }
 }
 
 def buildFunction = {
-    sh """#!/bin/bash
-        set -ex
+    withCredentials([
+        string(credentialsId: 'HydrosphereDevEndpoint', variable: 'HYDROSPHERE_ENDPOINT'),
+        string(credentialsId: 'HydroIntegrationsAWSRegion', variable: 'AWS_REGION'),
+        string(credentialsId: 'HydroIntegrationsS3DataCaptureBucket', variable: 'S3_DATA_CAPTURE_BUCKET'),
+        string(credentialsId: 'HydroIntegrationsS3DataCapturePrefix', variable: 'S3_DATA_CAPTURE_PREFIX'),
+        string(credentialsId: 'HydroIntegrationsS3DataTrainingBucket', variable: 'S3_DATA_TRAINING_BUCKET'),
+        string(credentialsId: 'HydroIntegrationsS3DataTrainingPrefix', variable: 'S3_DATA_TRAINING_PREFIX'),
+    ]) {
+        sh """#!/bin/bash
+            set -ex
 
-        # prepare environment
-        export AWS_REGION=us-west-3
-        
-        pyenv install --skip-existing 3.6.10
-        pyenv install --skip-existing 3.7.7
-        pyenv install --skip-existing 3.8.2
-        eval "\$(pyenv init -)"
-        pyenv shell 3.6.10 3.7.7 3.8.2
-        
-        python -m venv venv
-        source venv/bin/activate
-        pip install wheel~=0.34.2
-        pip install tox~=3.14.5
+            # prepare environment
+            pyenv install --skip-existing 3.6.10
+            pyenv install --skip-existing 3.7.7
+            pyenv install --skip-existing 3.8.2
+            eval "\$(pyenv init -)"
+            pyenv shell 3.6.10 3.7.7 3.8.2
+            
+            python -m venv venv
+            source venv/bin/activate
+            pip install wheel~=0.34.2
+            pip install tox~=3.14.5
 
-        # run tests for lambda distribution
-        # and for hydro-integrations sdk
-        tox
+            # run tests for lambda distribution
+            # and for hydro-integrations sdk
+            tox
 
-        # check that lambda distribution
-        # can be built
-        cd scrips/aws/traffic_shadowing
-        ./sam-build.sh
-        ./sam-package.sh
-        cd ../../../
+            # check that lambda distribution
+            # can be built
+            cd scrips/aws/traffic_shadowing
+            ./sam-build.sh
+            ./sam-package.sh
+            cd ../../../
 
-        # check that hydro-integrations
-        # sdk can be built
-        pip install -r requirements.txt
-        python setup.py bdist_wheel
-        deactivate
-    """
+            # check that hydro-integrations
+            # sdk can be built
+            pip install -r requirements.txt
+            python setup.py bdist_wheel
+            deactivate
+        """
+    }
 }
 
 def collectTestResults = {
